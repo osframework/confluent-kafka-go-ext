acac5e4d01184efd4592e95b552eed2bd952cbd5
Dave Joyce <dave@osframework.org>
2020-06-23 11:58:18 -0400

    #6 Added copyright header to source files, tweaked README

--- kafka/adminapi.go
+++ kafka/adminapi.go
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2020 OSFramework Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Package kafka provides interfaces extracted from the core API struct types
+// in the Confluent Kafka Golang client; see https://github.com/confluentinc/confluent-kafka-go
 package kafka
 
 import (
@@ -5,6 +23,7 @@ import (
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
+// Extracted Kafka admin client interface for purposes of composition and mocking
 type AdminClient interface {
 	ClusterID(ctx context.Context) (clusterID string, err error)
 	ControllerID(ctx context.Context) (controllerID int32, err error)
